4f7a8f79cdcb723f0e6997a7fb6a979c317d7ce7
Parker Wallace <wallace.parker@hotmail.com>
2023-11-05 11:37:20 -0330

    added mine generation mines generate in an array and are cross referenced with buttons

--- minesweeper.js
+++ minesweeper.js
@@ -3,29 +3,44 @@ const $ = (selector) => document.querySelector(selector);
 canvas = $("#gameboard")
 function gamestart (evt) {
 
+//variables
+const size = Math.sqrt(evt.target.dataset.size)
+const minecount = evt.target.dataset.bombs
+
 // initializing the game timer    
 setInterval(()=> {
 const timerbox = $('#gametimer');
 timerbox.textContent = ('000' + (parseInt(timerbox.textContent) + 1)).slice(-3)}, 1000)
 
+// generate the mines
+let mines = []
+let max = Math.round(size/2)
+let min = Math.round(size/2) - size
+for (let i = 0; i < minecount; i++) {
+    do {
+        x = Math.floor(Math.random() * (max - (min + 1) ) + (min));
+        y = Math.floor(Math.random() * (max - (min + 1) ) + (min));
+    } while (mines.some(mine => mine.x === x && mine.y === y));
+    mines.push({ x, y })
+};
+
 //create the board
-for (let i = 0; i < Math.sqrt(evt.target.dataset.size); i++) {
-    for (let j = 0; j < Math.sqrt(evt.target.dataset.size); j++) {
+for (let i = Math.round(size/2) - size; i < Math.round(size/2); i++) {
+    for (let j = Math.round(size/2) - size; j < Math.round(size/2); j++) {
       const node = document.createElement("button");
-      node.textContent = "0"; //used instead of "#" for visibility
-      node.dataset.coordinates = [i, j];
+      mines.some(mine => mine.x == i && mine.y == j)? node.textContent = "-1": node.textContent = "0"; // Set "M" for mine:
+
       $("#minefield").appendChild(node);
     }
-    $("#minefield").appendChild(document.createElement("br"));
-  }
+    $("#minefield").appendChild(document.createElement("br"));}
 
-};
+}
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
     let isGameOver = false;
-    const difficulty = [{"difficulty":"easy", "size":81, "bombs":10},{"difficulty":"medium", "size":256, "bombs":10},{"difficulty":"hard", "size":480, "bombs":10}]
+    const difficulty = [{"difficulty":"easy", "size":81, "bombs":10},{"difficulty":"medium", "size":256, "bombs":40},{"difficulty":"hard", "size":480, "bombs":99}]
     
     difficulty.forEach((value)=> {
     let button = document.createElement("button")
